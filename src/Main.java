50738d04dc7e57d127feaeccbdb5e18bb79e59a8
kamil116rus <kh8@rambler.ru>
2023-06-21 01:56:43 +0300

    home work

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,22 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
-        System.out.println((115/2)*4);
+        System.out.println((118/2)*4);
+
+        int lines = 6;
+        for (int line = 1; line < lines; line ++) {
+           print_line(lines - line, (line * 2) - 1);
+        }
+        System.out.println("   ***");
+        System.out.println("   ***");
+    }
+    public static void print_line(int space, int star) {
+        for(int i = 1; i < space; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i< star; i++) {
+            System.out.print("+");
+        }
+        System.out.println("");
     }
 }
